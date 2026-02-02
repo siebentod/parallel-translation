06a1f57215640347c21c8238c6a2f2c0250928d0
siebentod <33163165+siebentod@users.noreply.github.com>
2026-02-02 14:19:58 +0300

    fix: smallfix window size

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,12 +1,6 @@
 // src/lib.rs
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// use tauri::{AppHandle, Manager, State};
-use std::{
-    collections::HashMap,
-    sync::{Arc, Mutex},
-};
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
